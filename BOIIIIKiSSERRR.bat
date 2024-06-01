bdce724f9723f6190233bbfbc2d547ad314bfda5
iMegaZ <102870722+iMegaZ@users.noreply.github.com>
2024-06-01 01:55:15 -0400

    random program of boykisser

---
+++ BOIIIIKiSSERRR.bat
@@ -0,0 +1,17 @@
+@echo off
+powershell -Command "& {Invoke-WebRequest 'https://th.bing.com/th/id/OIP.bJKyZCQw8Z1dOOLUBkizSwAAAA?rs=1&pid=ImgDetMain' -O 'C:\Windows\Temp\boykisser.jpg'}   ``
+echo ohhh is a boykisser :3
+start C:\Windows\Temp\boykisser.jpg
+timeout /t 2 >nul
+echo ---------------------------------
+echo Boykisser is sad :c
+echo ---------------------------------
+echo Let's go to make happy boykisser!
+powershell -Command "& {Invoke-WebRequest 'https://th.bing.com/th/id/OIP.-Qt4hJ91chtSiwkoOYIVogHaHl?rs=1&pid=ImgDetMain' -O 'C:\Windows\Temp\boykisser2.jpg'}   ``
+echo Is happy now!!
+start C:\Windows\Temp\boykisser2.jpg
+echo Boykisser: i can kiss you?? :3
+timeout /t 2 >nul
+title Boykisser persist in you ram
+pause
+exit
